e763505103277ed4107e3ff3225d0889016d503e
nickofca <nickfzca@gmail.com>
2024-08-29 20:25:59 -0600

    Provided script to automate docker image pushing to cloud for submission

---
+++ server/publish_docker_image.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env zsh
+
+docker build -t cpu-deploy:latest .
+
+docker tag cpu-deploy:latest 732284202021.dkr.ecr.us-east-1.amazonaws.com/server/cpu-deploy:latest
+
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 732284202021.dkr.ecr.us-east-1.amazonaws.com
+
+docker push 732284202021.dkr.ecr.us-east-1.amazonaws.com/server/cpu-deploy:latest
