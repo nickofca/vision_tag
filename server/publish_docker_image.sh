cb676f560dd87f68d485576c12a207792976a9cb
nickofca <nickfzca@gmail.com>
2024-08-30 10:15:06 -0600

    Included comments for Dockerfile publish steps

--- server/publish_docker_image.sh
+++ server/publish_docker_image.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env zsh
 
+# Build the image for the specified platform using docker
 docker build --platform linux/amd64 -t cpu-deploy:latest .
 
+# Tag the image for ECR
 docker tag cpu-deploy:latest 732284202021.dkr.ecr.us-east-1.amazonaws.com/server/cpu-deploy:latest
 
+# Log in to AWS ECR (docker uses Docker's login mechanism, so this part remains the same)
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 732284202021.dkr.ecr.us-east-1.amazonaws.com
 
+# Push the image to AWS ECR
 docker push 732284202021.dkr.ecr.us-east-1.amazonaws.com/server/cpu-deploy:latest
