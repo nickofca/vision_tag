cd09d30baae66347f988805dea40c81a02c93ec6
nickofca <nickfzca@gmail.com>
2024-09-12 23:11:33 +0200

    Enable auth listener in full layout (ie root),

--- mobile/app/auth/_layout.tsx
+++ mobile/app/auth/_layout.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import { Stack } from 'expo-router';
-import AuthListener from '@hooks/AuthListener';
 import {DefaultTheme, ThemeProvider} from "@react-navigation/native"; // Import the AuthListener
 
 const AuthLayout: React.FC = () => {
     return (
         <>
-            <AuthListener />
             <ThemeProvider value={DefaultTheme}>
                 <Stack>
                     <Stack.Screen name="login" options={{ headerShown: false }} />
