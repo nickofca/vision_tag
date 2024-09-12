b4161b68752f4746f8269affbd25c2e6e940e40c
nickofca <nickfzca@gmail.com>
2024-09-12 23:17:39 +0200

    Altered routing logic to be more dynamic to current routes and reduce redundant routing calls.

--- mobile/hooks/AuthListener.tsx
+++ mobile/hooks/AuthListener.tsx
@@ -1,12 +1,12 @@
 import { useEffect, useRef } from 'react';
-import { useRouter, useRootNavigationState } from 'expo-router';
+import { useRouter } from 'expo-router';
 import { tokenStore } from '@services/auth'; // Import your Zustand store
+import { useRoute } from '@react-navigation/native';
 
 const AuthListener = () => {
     const token = tokenStore((state) => state.token); // Subscribe to the `token` state
     const router = useRouter(); // Use the expo-router hook to handle navigation
     const hasMounted = useRef(false); // Use a ref to track if the component has mounted
-
 
     useEffect(() => {
         if (!hasMounted.current) {
@@ -14,14 +14,16 @@ const AuthListener = () => {
             return;
         }
 
-        if (token) {
-            // If the token is set, redirect the user to the dashboard
+        const currentRoute = useRoute.name; // Get the current route
+        console.log(currentRoute)
+        if (token && currentRoute !== '/lobby/menu') {
+            // If the token is set and the user is not on the lobby menu, redirect to the dashboard
             router.replace('/lobby/menu');
-        } else {
-            // If the token becomes null or is not set, redirect the user to the login page
+        } else if (!token && currentRoute !== '/auth/login') {
+            // If the token is null or not set, and the user is not on the login page, redirect to login
             router.replace('/auth/login');
         }
-    }, [token]); // Trigger this effect whenever `token` or `router` changes
+    }, [token, router]); // Trigger this effect whenever `token` or `router` changes
 
     return null; // This component doesn’t need to render anything
 };
